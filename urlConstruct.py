e27a157098bb0e6bc81266b22e7a970aca1c30c6
wql_g <wql_g@qq.com>
2019-07-25 21:14:46 +0800

    fix some bugs

--- urlConstruct.py
+++ urlConstruct.py
@@ -2,7 +2,7 @@ import os
 
 
 av = input("请输入B战视频av号（avxxxxxxxx）：\n")
-num = input("how many videos in the playlist:\?")
+num = int(input("how many videos in the playlist:\n"))
 parentUrl = 'https://www.bilibili.com/video/'+av+'/?p='
 url = ''
 for i in range(1, num+1):
